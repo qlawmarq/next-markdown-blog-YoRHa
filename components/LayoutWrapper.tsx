2c9194ab460a3225ee40ec4a82daa498799c5b3b
Masaki Yoshiiwa <yopinoji@gmail.com>
2021-09-14 20:01:00 +0900

    fix build error on Vercel

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { siteMetadata } from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
 import Logo from '@/data/logo.svg'
-import Link from './Link'
-import SectionContainer from './SectionContainer'
-import Footer from './Footer'
-import MobileNav from './MobileNav'
-import ThemeSwitch from './ThemeSwitch'
+import Link from '@/components/Link'
+import SocialIcon from '@/components/social-icons'
+import SectionContainer from '@/components/SectionContainer'
+import MobileNav from '@/components/MobileNav'
+import ThemeSwitch from '@/components/ThemeSwitch'
 
 const LayoutWrapper: React.FC = ({ children }) => {
   return (
@@ -46,7 +46,30 @@ const LayoutWrapper: React.FC = ({ children }) => {
           </div>
         </header>
         <main className="mb-auto">{children}</main>
-        <Footer />
+        <footer>
+          <div className="flex flex-col items-center mt-16">
+            <div className="flex mb-3 space-x-4">
+              <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
+              <SocialIcon kind="github" href={siteMetadata.github} size={6} />
+              <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
+              <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
+              <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
+              <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
+            </div>
+            <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+              <div>{siteMetadata.author}</div>
+              <div>{` • `}</div>
+              <div>{`© ${new Date().getFullYear()}`}</div>
+              <div>{` • `}</div>
+              <Link href="/">{siteMetadata.title}</Link>
+            </div>
+            <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+              <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
+                Tailwind Nextjs Theme
+              </Link>
+            </div>
+          </div>
+        </footer>
       </div>
     </SectionContainer>
   )
