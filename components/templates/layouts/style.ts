4c879e96d3db9d39fc925243c588a2458d61734d
Masaki Yoshiiwa <yopinoji@gmail.com>
2021-09-20 22:48:05 +0900

    update styles

--- components/templates/layouts/style.ts
+++ components/templates/layouts/style.ts
@@ -1,6 +1,12 @@
 import { css, keyframes } from '@emotion/react'
 import { fontSize } from '@/constants/styles'
-import { H1Style, H2Style, H3Style, AnchorStyle } from '@/components/atoms/Typography'
+import {
+  H1Style,
+  H2Style,
+  H3Style,
+  AnchorStyle,
+  BlockquoteStyle,
+} from '@/components/atoms/Typography'
 
 const fadeIn = keyframes`
   0% {
@@ -61,24 +67,7 @@ export const PostLayoutStyle = css`
   }
 
   .post-contents blockquote {
-    position: relative;
-    margin: 1rem 0;
-    padding: 0.5rem 1.5rem;
-    box-sizing: border-box;
-    font-style: italic;
-  }
-
-  .post-contents blockquote::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    height: 100%;
-    width: 0.3rem;
-    padding: 0.15rem;
-    border: solid #bab5a1;
-    border-width: 0 0.2rem 0 0.6rem;
+    ${BlockquoteStyle}
   }
 
   .post-contents ul,
