c4449a44036ea4aa38e8fb4057a5064780dfa0b6
Masaki Yoshiiwa <yopinoji@gmail.com>
2021-09-20 23:12:19 +0900

    add slidein animation

--- components/atoms/Section/style.ts
+++ components/atoms/Section/style.ts
@@ -1,5 +1,16 @@
 import { css, keyframes } from '@emotion/react'
 
+const slideIn = keyframes`
+    0% {
+        opacity: 0;
+        transform: translateX(25px);
+    }
+    100% {
+        opacity: 1;
+        transform: translateX(0px);
+    }
+`
+
 export const style = css`
   position: relative;
   flex-direction: column;
@@ -8,6 +19,7 @@ export const style = css`
   padding: 0.5rem;
   padding-left: 1.5rem;
   box-shadow: 0.2em 0.2em #bab5a1;
+  animation: ${slideIn} 0.5s ease-in-out 0s 1 normal;
   &:before {
     content: '';
     position: absolute;
@@ -18,6 +30,7 @@ export const style = css`
     padding: 0.15rem;
     border: solid #454138;
     border-width: 0 0.2rem 0 0.6rem;
+    transition: all 0.2s ease-out;
   }
   &:hover:before {
     border: solid #bab5a1;
--- components/atoms/Typography/Anchor/style.ts
+++ components/atoms/Typography/Anchor/style.ts
@@ -3,6 +3,7 @@ import { css } from '@emotion/react'
 export const style = css`
   text-decoration: underline;
   cursor: pointer;
+  transition: all 0.2s ease-out;
   &:hover {
     opacity: 0.7;
   }
