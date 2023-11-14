850c71f7724aef0da94e29483fc471ed32d93559
Masaki <masaki.yoshiiwa@gmail.com>
2023-11-14 17:44:10 +0900

    feat: add span component

--- components/molecules/Tag/index.tsx
+++ components/molecules/Tag/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Anchor } from '@/components/atoms/Anchor'
 import { style } from './style'
+import { Span } from '@/components/atoms/Typography'
 
 const Tag: React.FC<{ href?: string; text: string }> = ({ href, text }) => {
   if (!href) {
-    return <span css={style}>{text}</span>
+    return <Span css={style}>{text}</Span>
   }
   return (
-    <span css={style}>
+    <Span css={style}>
       <Anchor href={href}>{text}</Anchor>
-    </span>
+    </Span>
   )
 }
 
