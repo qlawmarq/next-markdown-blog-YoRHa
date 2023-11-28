4cb9e27947f5d1a55934e9738a0d5fa09ba6ea93
Masaki <masaki.yoshiiwa@gmail.com>
2023-11-28 18:20:50 +0900

    chore: add error handling in getAllTags

--- lib/tags/tags.ts
+++ lib/tags/tags.ts
@@ -1,12 +1,17 @@
 import { getAllFilesFrontMatter } from '../markdown'
 
 export async function getAllTags(folder: string) {
-  const frontMatters = await getAllFilesFrontMatter(folder)
-  let tags: string[] = []
-  frontMatters
-    .filter((frontmatter) => !frontmatter.draft && frontmatter.tags)
-    .forEach((frontmatter) => {
-      frontmatter.tags?.forEach((tag) => tags.push(tag))
-    })
-  return [...new Set(tags)]
+  try {
+    const frontMatters = await getAllFilesFrontMatter(folder)
+    const tags: string[] = []
+    frontMatters
+      .filter((frontmatter) => !frontmatter.draft && frontmatter.tags)
+      .forEach((frontmatter) => {
+        frontmatter.tags?.forEach((tag) => tags.push(tag))
+      })
+    return [...new Set(tags)]
+  } catch (error) {
+    console.error('Error in getAllTags', error)
+    return []
+  }
 }
